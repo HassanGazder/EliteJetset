5cd5490a97676c439173f184277baa9fa0206327
HassanGazder <hassan2110c@aptechgdn.net>
2025-04-26 22:37:01 +0500

    vercel deployment integrated

---
+++ backend/api/index.js
@@ -0,0 +1,5 @@
+const app = require('../server'); // adjust path if needed
+
+module.exports = (req, res) => {
+  app(req, res);
+};
--- backend/server.js
+++ backend/server.js
@@ -61,6 +61,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-}); 
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// }); 
+module.exports = app;
